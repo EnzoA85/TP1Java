85b7e62e0b030d536bfebdc0de7bfbafab242a52
etud user <etud@port-hp-probook650G8-1>
2023-11-10 15:07:27 +0100

    add jeu de test

--- src/App.java
+++ src/App.java
@@ -1,5 +1,22 @@
 public class App {    
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        voiture voiture1 = new voiture(10000,"Peugeot", 2010, "AA-123-AA", "B", true);
+        voiture voiture2 = new voiture(25000, "Renault", 2015, "BB-123-BB", "B", false);
+
+        autocars autocar1 = new autocars(100000,"Mercedes", 2010, "EE-123-EE", "D", 100, 50);
+        autocars autocar2 = new autocars(150000,"Iveco", 2015, "FF-123-FF", "D", 150, 75);
+
+        camion camion1 = new camion(100000,"Volvo", 2010,"GG-123-GG", "C", 100);
+
+        System.out.println("Voiture 1 :");
+        voiture1.afficherVoiture();
+        System.out.println("Voiture 2 :");
+        voiture2.afficherVoiture();
+        System.out.println("Voiture 3 :");
+        autocar1.afficherVehicule();
+        System.out.println("Voiture 4 :");
+        autocar2.afficherVehicule();
+        System.out.println("Voiture 5 :");
+        camion1.afficherVehicule();
     }
 }
